15366cbf39ac45e0d1ecc2c10c5696d5ed1aec7e
ronksc <ksc.ron@gmail.com>
2019-01-14 00:32:24 +0800

    ig popup

--- wp-content/themes/fao/templates/content-press.php
+++ wp-content/themes/fao/templates/content-press.php
@@ -41,9 +41,42 @@
 		</div>
 		<div class="col-12 col-md-9">
 			<div class="row module__instagram-gallery">
-				<div class="col-md-4"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/img_ig_1.jpg" class="img-fluid" /></a></div>
-				<div class="col-md-4"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/img_ig_2.jpg" class="img-fluid" /></a></div>
-				<div class="col-md-4"><a href="#"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/img_ig_3.jpg" class="img-fluid" /></a></div>
+				<div class="col-md-4"><a data-fancybox data-src="#hidden-content1" href="javascript:;"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/img_ig_1.jpg" class="img-fluid" /></a></div>
+				<div class="instagram-popup" id="hidden-content1">
+					<div class="row">
+						<div class="col-md-6">
+							<a href="https://www.instagram.com/p/BmICEZ1hn9i/?taken-by=eqiq_pure" target="_blank"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/eqiq_pure_FW18_website_ig_01.jpg" class="img-fluid" /></a>
+						</div>
+						<div class="col-md-6">
+							<p class="p1">eqiq_purePlaid fever .</p>
+							<p><span><a href="https://www.instagram.com/p/BmICEZ1hn9i/?taken-by=eqiq_pure" target="_blank" class="instaname">@eqiq_pure</a></span> // <span class="fdate">17/08/2018</span></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4"><a data-fancybox data-src="#hidden-content2" href="javascript:;"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/img_ig_2.jpg" class="img-fluid" /></a></div>
+				<div class="instagram-popup" id="hidden-content2">
+					<div class="row">
+						<div class="col-md-6">
+							<a href="https://www.instagram.com/p/BmICEZ1hn9i/?taken-by=eqiq_pure" target="_blank"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/eqiq_pure_FW18_website_ig_01.jpg" class="img-fluid" /></a>
+						</div>
+						<div class="col-md-6">
+							<p class="p1">eqiq_purePlaid fever .</p>
+							<p><span><a href="https://www.instagram.com/p/BmICEZ1hn9i/?taken-by=eqiq_pure" target="_blank" class="instaname">@eqiq_pure</a></span> // <span class="fdate">17/08/2018</span></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4"><a data-fancybox data-src="#hidden-content3" href="javascript:;"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/img_ig_3.jpg" class="img-fluid" /></a></div>
+				<div class="instagram-popup" id="hidden-content3">
+					<div class="row">
+						<div class="col-md-6">
+							<a href="https://www.instagram.com/p/BmICEZ1hn9i/?taken-by=eqiq_pure" target="_blank"><img src="<?=get_stylesheet_directory_uri()?>/assets/images/eqiq_pure_FW18_website_ig_01.jpg" class="img-fluid" /></a>
+						</div>
+						<div class="col-md-6">
+							<p class="p1">eqiq_purePlaid fever .</p>
+							<p><span><a href="https://www.instagram.com/p/BmICEZ1hn9i/?taken-by=eqiq_pure" target="_blank" class="instaname">@eqiq_pure</a></span> // <span class="fdate">17/08/2018</span></p>
+						</div>
+					</div>
+				</div>
 			</div>
 			<div class="module__instagram-social text-right">
 				<ul>
